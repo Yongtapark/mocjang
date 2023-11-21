712a748d81dece0a5223e71737f6ec6ff71ba8b5
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-21 22:41:17 +0900

    feat : 부모 소 추가

--- src/main/java/cow/mocjang/domain/cow/Gene.java
+++ src/main/java/cow/mocjang/domain/cow/Gene.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,4 +16,8 @@ public class Gene {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String codename;
+    @OneToOne
+    private Cow fatherCow;
+    @OneToOne
+    private Cow motherCow;
 }
