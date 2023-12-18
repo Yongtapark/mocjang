f757e6ea89064b49451afe2817b7bba156b6c2e8
Yong Ta Park <qkrdydxk80@nate.com>
2023-12-18 14:15:48 +0900

    feat : 파싱로직 추가

--- src/main/java/cow/mocjang/core/enums/EnMockJang.java
+++ src/main/java/cow/mocjang/core/enums/EnMockJang.java
@@ -1,5 +1,9 @@
 package cow.mocjang.core.enums;
 
+import cow.mocjang.core.exceptions.IllegalNoteFormatException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum EnMockJang {
@@ -18,8 +22,8 @@ public enum EnMockJang {
         return compile;
     }
 
-    public boolean isSameType(EnMockJang enMockJang) {
-        return this == enMockJang;
+    public boolean isPatternMatched(String strPattern) {
+        return getCompile().matcher(strPattern).find();
     }
 
     public EnMockJang compareTypeWithPattern(String name) {
@@ -27,5 +31,21 @@ public enum EnMockJang {
             return this;
         }
         return NONE;
+    }
+
+    public Map<EnMockJang, Map<String, String>> parseAndAddToMap(String ids, String value,
+                                                                        Map<EnMockJang, Map<String, String>> mockJangMapHashMap) {
+        Matcher matcher = getCompile().matcher(ids);
+        if (matcher.find()) {
+            String[] idArray = ids.split(",");
+            Map<String, String> currentMap = mockJangMapHashMap.computeIfAbsent(this, k -> new HashMap<>());
+            for (String id : idArray) {
+                if (currentMap.containsKey(id)) {
+                    throw new IllegalNoteFormatException(id);
+                }
+                currentMap.put(id, value);
+            }
+        }
+        return mockJangMapHashMap;
     }
 }
